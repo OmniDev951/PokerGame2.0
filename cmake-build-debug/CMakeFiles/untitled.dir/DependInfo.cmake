e72819649b3ef972997a27215b04b78ab7766fe1
OmniDev951 <ochsjax000@alpinesd.org>
2025-01-14 09:31:03 -0700

    Test
    Opened at school purely a test. Will check at home later

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ochsjax000/Documents/GitHub/PokerGame2.0/AIPlayer.cpp" "CMakeFiles/untitled.dir/AIPlayer.cpp.obj" "gcc" "CMakeFiles/untitled.dir/AIPlayer.cpp.obj.d"
+  "C:/Users/ochsjax000/Documents/GitHub/PokerGame2.0/AiOpponent.cpp" "CMakeFiles/untitled.dir/AiOpponent.cpp.obj" "gcc" "CMakeFiles/untitled.dir/AiOpponent.cpp.obj.d"
+  "C:/Users/ochsjax000/Documents/GitHub/PokerGame2.0/DeckOfCards.cpp" "CMakeFiles/untitled.dir/DeckOfCards.cpp.obj" "gcc" "CMakeFiles/untitled.dir/DeckOfCards.cpp.obj.d"
+  "C:/Users/ochsjax000/Documents/GitHub/PokerGame2.0/GameLoop.cpp" "CMakeFiles/untitled.dir/GameLoop.cpp.obj" "gcc" "CMakeFiles/untitled.dir/GameLoop.cpp.obj.d"
+  "C:/Users/ochsjax000/Documents/GitHub/PokerGame2.0/Player.cpp" "CMakeFiles/untitled.dir/Player.cpp.obj" "gcc" "CMakeFiles/untitled.dir/Player.cpp.obj.d"
+  "C:/Users/ochsjax000/Documents/GitHub/PokerGame2.0/PlayingCard.cpp" "CMakeFiles/untitled.dir/PlayingCard.cpp.obj" "gcc" "CMakeFiles/untitled.dir/PlayingCard.cpp.obj.d"
+  "C:/Users/ochsjax000/Documents/GitHub/PokerGame2.0/main.cpp" "CMakeFiles/untitled.dir/main.cpp.obj" "gcc" "CMakeFiles/untitled.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/AIPlayer.cpp.obj"
+  "CMakeFiles/untitled.dir/AIPlayer.cpp.obj.d"
+  "CMakeFiles/untitled.dir/AiOpponent.cpp.obj"
+  "CMakeFiles/untitled.dir/AiOpponent.cpp.obj.d"
+  "CMakeFiles/untitled.dir/DeckOfCards.cpp.obj"
+  "CMakeFiles/untitled.dir/DeckOfCards.cpp.obj.d"
+  "CMakeFiles/untitled.dir/GameLoop.cpp.obj"
+  "CMakeFiles/untitled.dir/GameLoop.cpp.obj.d"
+  "CMakeFiles/untitled.dir/Player.cpp.obj"
+  "CMakeFiles/untitled.dir/Player.cpp.obj.d"
+  "CMakeFiles/untitled.dir/PlayingCard.cpp.obj"
+  "CMakeFiles/untitled.dir/PlayingCard.cpp.obj.d"
+  "CMakeFiles/untitled.dir/main.cpp.obj"
+  "CMakeFiles/untitled.dir/main.cpp.obj.d"
+  "libuntitled.dll.a"
+  "untitled.exe"
+  "untitled.exe.manifest"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
